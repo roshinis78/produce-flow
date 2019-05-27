43de5ae0e1daf4d654e0051cf68b508d93973fe4
Roshini Saravanakumar <roshini2@illinois.edu>
2019-05-26 23:43:51 -0700

    Read geojson + drawn map and axes :)

--- visualization.js
+++ visualization.js
@@ -1,5 +1,5 @@
 $(function () {
-  d3.csv('relevant_data.csv').then(function (data) {
+  d3.csv('data/relevant_data.csv').then(function (data) {
     console.log(data); // debugging
     visualize(data);
   });
@@ -8,9 +8,9 @@ $(function () {
 
 var visualize = function (data) {
   // Boilerplate:
-  var margin = { top: 50, right: 50, bottom: 50, left: 50 };
-  var width = 960 - margin.left - margin.right;
-  var height = 500 - margin.top - margin.bottom;
+  var margin = { top: 0, right: 0, bottom: 50, left: 60 };
+  var width = 1100 - margin.left - margin.right;
+  var height = 600 - margin.top - margin.bottom;
 
   var svg = d3.select('#chart')
     .append('svg')
@@ -20,9 +20,47 @@ var visualize = function (data) {
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
   // Visualization Code:
+
+  // world map
+  var worldGeoJSON = require('./data/countries.json')
+  var projection = d3.geoEquirectangular()
+  var pathGenerator = d3.geoPath(projection)
+
   svg
-    .append('text')
-    .attr('x', 0)
-    .attr('y', 0)
-    .text('Visualization goes here!')
+    .selectAll('countries')
+    .data(worldGeoJSON.features)
+    .enter()
+    .append('path')
+    .attr('d', pathGenerator) // d attribute defines the shape of the path
+    .style('stroke', 'white')
+    .style('fill', '#81D4FA')
+
+  var map = {
+    top: 0,
+    right: 960,
+    left: 0,
+    bottom: 490
+  }
+
+  // axes
+  var latitudeScale = d3
+    .scaleLinear()
+    .domain([-90, 90])
+    .range([map.bottom, map.top])
+  var latitudeAxis = d3.axisLeft(latitudeScale)
+
+  var longitudeScale = d3
+    .scaleLinear()
+    .domain([-180, 180])
+    .range([map.left, map.right])
+  var longitudeAxis = d3.axisBottom(longitudeScale)
+
+  svg
+    .append('g')
+    .call(latitudeAxis)
+
+  svg
+    .append('g')
+    .call(longitudeAxis)
+    .attr('transform', 'translate(0,' + map.bottom + ')')
 };
