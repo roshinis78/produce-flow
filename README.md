ad38da4580282652ea268cc7fe79042332cde939
Roshini Saravanakumar <roshini2@illinois.edu>
2019-05-27 17:39:25 -0700

    Working on tooltips

--- README.md
+++ README.md
@@ -9,3 +9,6 @@
 <a href='https://github.com/datasets/country-list'>List of Countries Worldwide</a>
 - Used this dataset as a lookup table for countries when cleaning the produce flow dataset.
 - Modified some country names to update formatting and appended any missing countries.
+
+<a href='https://community.periscopedata.com/t/63fy7m/country-centroids'>Country Centroids</a>
+- Used this dataset as a lookup table for countries and their centroids.
--- visualization.js
+++ visualization.js
@@ -1,10 +1,27 @@
+var Popper = require('popper.js')
+var locationLookup = {}
+
 $(function () {
-  d3.csv('data/relevant_data.csv').then(function (data) {
+  // intialize the location lookup table
+  d3.csv('data/countries_lookup.csv').then(function (data) {
+    data.forEach(function (country, index) {
+      locationLookup[country.name] = { 'latitude': country.latitude, 'longitude': country.longitude }
+    })
+    console.log('Printing location lookup table...')
+    console.log(locationLookup)
+  });
+
+  d3.csv('data/avacados_2012_top10_export.csv').then(function (data) {
     console.log(data); // debugging
     visualize(data);
   });
 });
 
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+var viewClass = 'avacados_2012_top10_export'
 
 var visualize = function (data) {
   // Boilerplate:
@@ -25,6 +42,21 @@ var visualize = function (data) {
   var worldGeoJSON = require('./data/countries.json')
   var projection = d3.geoEquirectangular()
   var pathGenerator = d3.geoPath(projection)
+  var mapPalette = ['#a1c9f4',
+    '#ffb482',
+    '#8de5a1',
+    '#ff9f9b',
+    '#d0bbff',
+    '#debb9b',
+    '#fab0e4',
+    '#cfcfcf',
+    '#fffea3',
+    '#b9f2f0',
+    '#a1c9f4',
+    '#ffb482',
+    '#8de5a1',
+    '#ff9f9b',
+    '#d0bbff']
 
   svg
     .selectAll('countries')
@@ -33,13 +65,16 @@ var visualize = function (data) {
     .append('path')
     .attr('d', pathGenerator) // d attribute defines the shape of the path
     .style('stroke', 'white')
-    .style('fill', '#81D4FA')
+    .style('fill', function (d, i) {
+      // randomly select color from palette
+      return mapPalette[Math.floor(Math.random() * mapPalette.length)]
+    })
 
   var map = {
     top: 0,
     right: 960,
     left: 0,
-    bottom: 490
+    bottom: 489
   }
 
   // axes
@@ -63,4 +98,28 @@ var visualize = function (data) {
     .append('g')
     .call(longitudeAxis)
     .attr('transform', 'translate(0,' + map.bottom + ')')
+
+  // read dataset, add a circle for each element
+  svg
+    .selectAll('exercise')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('r', function (d, i) {
+      var tooltip = new Tooltip(this, {
+        title: "Hello roshini :)",
+        placement: "bottom"
+      })
+      return 2
+    })
+    .attr('cx', function (d, i) {
+      return longitudeScale(locationLookup[d['Country']].longitude)
+    })
+    .attr('cy', function (d, i) {
+      return latitudeScale(locationLookup[d['Country']].latitude)
+    })
+    .attr('class', viewClass)
+
+  // tooltips
+
 };
