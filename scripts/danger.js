78ec6a6f3381c485e47331c42c44e1befdaf6b24
Roshini Saravanakumar <roshini2@illinois.edu>
2019-06-03 13:50:29 -0700

    Danger Zone intro + basic bar graph viz working

--- scripts/danger.js
+++ scripts/danger.js
@@ -4,6 +4,16 @@ var year = '2006'
 // For bar graphs
 $(function () {
   d3.csv('../data/percent_consumption.csv').then(function (data) {
+    // populate the country selector options with the countries
+    countries = Array.from(new Set(data.map(entry => entry['Country'])))
+    countrySelect = document.getElementById('country-select')
+    countries.forEach(function (country) {
+      var option = document.createElement('option')
+      option.setAttribute('value', country)
+      option.innerHTML = country;
+      countrySelect.appendChild(option)
+    })
+
     // only select data relevant to the country and year in question
     data = data.filter(entry => ((entry['Country'] == country) && (entry['Year'] == year)))
     console.log('Printing data for ' + country + ' from ' + year)
@@ -15,9 +25,9 @@ $(function () {
 })
 
 function stackedBarVisualize(data) {
-  const margin = { top: 10, right: 50, bottom: 20, left: 80 }
+  const margin = { top: 10, right: 20, bottom: 30, left: 115 }
   const width = 1000
-  const height = 490
+  const height = 1000
 
   var svg = d3.select('#bar-chart')
     .append('svg')
@@ -50,4 +60,36 @@ function stackedBarVisualize(data) {
     .append('g')
     .call(percentAxis)
     .attr('transform', 'translate(0,' + height + ')')
+
+  // bars in bar graph
+  var bar = {
+    height: 15,
+    color: {
+      normal: '#ffd633',
+      dangerZone: '#ff3300'
+    }
+  }
+
+  const dangerZoneThreshold = 20
+  svg
+    .selectAll('bars')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('x', margin.left)
+    .attr('y', function (d, i) {
+      var firstFewWords = ((d['Produce']).split(','))[0]
+      return produceScale(firstFewWords) - (bar.height / 2)
+    })
+    .attr('width', function (d, i) {
+      console.log(d['Percent Consumed'] + ' -> ' + percentScale(d['Percent Consumed']))
+      return percentScale(d['Percent Consumed']) - margin.left
+    })
+    .attr('height', bar.height)
+    .attr('fill', function (d, i) {
+      if (d['Percent Consumed'] < dangerZoneThreshold) {
+        return bar.color.dangerZone
+      }
+      return bar.color.normal
+    })
 }
